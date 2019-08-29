37f2fcc8e419065f7eefd49881d83c2c07786db6
carlosilveraxe <carlosilveraxe@gmail.com>
2019-08-29 16:12:32 +0200

    Working on modal image function

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,8 +7,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'instant-messaging-app';
+  modal = document.getElementById('myModal') as HTMLElement;
+  img = document.getElementById('profile-picture') as HTMLElement;
+  modalImg = document.getElementById('img01') as HTMLElement;
 
   videoCalling() {
     window.location.href = 'https://youtu.be/-HrPr6IQNac?t=25';
+  }
+
+  enlargeImg() {
+    this.modal.style.display = 'Block';
   }
 }
