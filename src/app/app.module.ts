568f871158dcda0bd8ea20b01a62c26367ef755f
carlosilveraxe <carlosilveraxe@gmail.com>
2019-09-02 16:32:40 +0200

    Added Multimedia component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { VideoCallingComponent } from './video-calling/video-calling.component';
+import { MultimediaComponent } from './multimedia/multimedia.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     VideoCallingComponent,
+    MultimediaComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/multimedia/multimedia.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-multimedia',
+  templateUrl: './multimedia.component.html',
+  styleUrls: ['./multimedia.component.scss']
+})
+export class MultimediaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
