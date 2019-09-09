1087b7b3e7ef1df8e2781879e40bdea738bfe9f3
carlosilveraxe <carlosilveraxe@gmail.com>
2019-09-09 16:06:58 +0200

    Changed msgID to type string

--- src/app/chat-window/chat-window.component.ts
+++ src/app/chat-window/chat-window.component.ts
@@ -14,7 +14,7 @@ import {FirebaseService} from '../shared/firebase.service';
   styleUrls: ['./chat-window.component.scss']
 })
 export class ChatWindowComponent implements OnInit {
-  thisUserID = 1234;
+  thisUserID = '1234';
   title = 'instant-messaging-app';
   modal = document.getElementById('myModal') as HTMLImageElement;
   img = document.getElementById('profile-picture') as HTMLImageElement;
