2e18b56656bd700d391b737bf699e48b127a301d
Keanu Jooste <keanujooste000@gmail.com>
2019-09-09 08:36:08 +0000

    Added back end contacts functionality

--- src/app/chat-window/chat-window.component.ts
+++ src/app/chat-window/chat-window.component.ts
@@ -6,6 +6,7 @@ import {Message} from '@angular/compiler/src/i18n/i18n_ast';
 import { MatDialog, MatSnackBar } from '@angular/material';
 import { MultimediaComponent } from '../multimedia/multimedia.component';
 import {VideoCallingComponent} from '../video-calling/video-calling.component';
+import {FirebaseService} from '../shared/firebase.service';
 
 @Component({
   selector: 'app-chat-window',
@@ -21,8 +22,13 @@ export class ChatWindowComponent implements OnInit {
   input; filter; ul; li; a; i; txtValue;
   newMessage: string;
   public msgTime: Date = new Date();
+  private openChatIds: string[];
+  private openChatUserIds: string[];
 
-  users: Users[] = [
+  users: Users[] = [];
+  message: Messages[] = [];
+
+  /*users: Users[] = [
     new Users('0765843028', 'Keanu Jooste', 'online', 'https://scontent-jnb1-1.xx.fbcdn.net/v/t1.0-9/60937380_2227964937285183_6900601698239119360_n.jpg?_nc_cat=106&_nc_oc=AQmjhYtcyLXTRQ9EjwQGp70e3OYU16x3YMCc7ODLc-uvgPqj_BBvs-T6p5EsgzeoTho&_nc_ht=scontent-jnb1-1.xx&oh=9c51d38a12ea4a82dc6aeea28465c637&oe=5E10F323', 'online_icon'),
     new Users('0713304416', 'Jeandre Pretorius', 'left 7 mins ago', 'https://scontent-jnb1-1.xx.fbcdn.net/v/t1.0-9/19904938_1911186905828058_7381270267279841437_n.jpg?_nc_cat=100&_nc_oc=AQk0FJdBM1FmI9KAtrOOyLOxmKXV8rzdBkoERbDf-yk-LBsac1A5ldVwbLej1TZAI3k&_nc_ht=scontent-jnb1-1.xx&oh=37bebc49635c6f06cbdf814f9a1ad4ae&oe=5E0C581E', 'online_icon offline'),
     new Users('0731102348', 'Stiaan Bouwer', 'online', 'https://scontent-jnb1-1.xx.fbcdn.net/v/t1.0-9/16938651_1420230568048345_476885994405757357_n.jpg?_nc_cat=108&_nc_oc=AQlqb8ODhECeR4rP0EjCsIOLNEdv22u9fS3CAUDaTtj0tM_UVTBxp8rb8PBAYbPcm-U&_nc_ht=scontent-jnb1-1.xx&oh=8735366cef0719e77c8139048879ee0e&oe=5E04B854', 'online_icon'),
@@ -35,12 +41,15 @@ export class ChatWindowComponent implements OnInit {
     new Messages(2222, 'msg2', 'msgText', '14:29'),
     new Messages(1234, 'msg3', 'msgText', '14:30'),
     new Messages(3211, 'msg4', 'msgText', '14:30'),
-  ];
+  ];*/
 
   // Get the <span> element that closes the modal
   span = document.getElementsByClassName('close')[0];
 
-  constructor(private dialog: MatDialog, private snackBar: MatSnackBar) { }
+  constructor(private dialog: MatDialog, private snackBar: MatSnackBar,
+              private firebaseService: FirebaseService) {
+
+  }
 
   // Just to give info the application user
   openSnackBar(message: string, action: string) {
@@ -113,6 +122,10 @@ export class ChatWindowComponent implements OnInit {
 
   ngOnInit() {
     this.openSnackBar('Login Successful', 'close');
+    // this.openChatUserIds = this.firebaseService.getOpenChatUID(localStorage.getItem('currentUserId'));
+    // this.openChatIds = this.firebaseService.openChatChatIds;
+    this.users = this.firebaseService.getUserProfiles();
+    this.message = this.firebaseService.getMessages(this.openChatIds);
   }
 
   onGenerateNewMessage() {
--- src/app/shared/firebase.service.ts
+++ src/app/shared/firebase.service.ts
@@ -1,11 +1,19 @@
 import { Injectable } from '@angular/core';
-import { AngularFirestore } from '@angular/fire/firestore';
+import {AngularFirestore, DocumentData} from '@angular/fire/firestore';
 import {Router} from '@angular/router';
+import { Messages } from './messages.model';
+import { Users } from './users.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FirebaseService {
+  public openChatChatIds: string[] = [];
+  public openChatUserIds: string[] = [];
+  private userProfilesArr: Users[] = [];
+  private messagesArr: Messages[] = [];
+  private i: number;
+  private userIdArr: string[] = [];
 
   constructor(public db: AngularFirestore,
               private router: Router) {}
@@ -19,6 +27,9 @@ export class FirebaseService {
         } else {
           if (doc.data().password === password) {
             this.router.navigate(['chatWindow']);
+            localStorage.setItem('currentUserId', userId);
+            /*const arr = this.getOpenChats(userId);
+            console.log(arr);*/
           }
         }
       }).catch(err => {
@@ -43,6 +54,10 @@ export class FirebaseService {
     usersRef.set(data);
   }
 
+  getMessages(openChatIds: any[]): Messages[] {
+    return this.messagesArr;
+  }
+
   uploadImage(userId, contactId, Base64String, Filename) {
     // Upload Image
   }
@@ -53,5 +68,44 @@ export class FirebaseService {
 
   uploadAudio(userId, contactId, Base64String, Filename) {
     // Upload Audio
+  }
+
+  getUserProfiles(): Users[] {
+    const userRef = this.db.collection('users').doc(localStorage.getItem('currentUserId'));
+    const getDoc = userRef.get().toPromise()
+      .then(doc => {
+        if (!doc.exists) {
+          console.log('not found'); // add toastr notification
+        } else {
+          this.openChatUserIds = doc.data().openChatUserIds;
+          for (let i = 0; i < this.openChatUserIds.length; i++) {
+            const ref = this.db.collection('users').doc(this.openChatUserIds[i]);
+            const document = ref.get().toPromise()
+              .then(udoc => {
+                if (!udoc.exists) {
+                  console.log('not found'); // add toastr notification
+                } else {
+                  this.userProfilesArr.push(new Users(
+                    udoc.data().userId,
+                    udoc.data().name,
+                    udoc.data().status,
+                    udoc.data().profilePicture,
+                    udoc.data().onlineIcon));
+                  /*this.userProfilesArr[i].name = udoc.data().name;
+                  this.userProfilesArr[i].onlineIcon = udoc.data().onlineIcon;
+                  this.userProfilesArr[i].profilePicture = udoc.data().profilePicture;
+                  this.userProfilesArr[i].status = udoc.data().status;
+                  this.userProfilesArr[i].userID = udoc.data().userID;*/
+                }
+              }).catch(err => {
+                console.log('Error', err); // add toastr notification
+              });
+          }
+        }
+      }).catch(err => {
+        console.log('Error', err); // add toastr notification
+      });
+    console.log(this.userProfilesArr);
+    return this.userProfilesArr;
   }
 }
