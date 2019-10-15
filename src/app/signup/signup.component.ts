56b25eaeedb26d639de481ba49fd0315db5bed5b
Ernst Blignaut <ernst.blignaut0@gmail.com>
2019-10-15 14:52:20 +0200

    Auto stash before merge of "contacts-panel-design" and "origin/contacts-panel-design"

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {NgForm} from '@angular/forms';
 import {FirebaseService} from '../shared/firebase.service';
 import {EncoderService} from '../shared/Encoder.service';
-import {MatDialogRef} from '@angular/material';
+import {MatDialogRef, MatSnackBar} from '@angular/material';
 import {MultimediaComponent} from '../multimedia/multimedia.component';
 
 @Component({
@@ -13,7 +13,7 @@ import {MultimediaComponent} from '../multimedia/multimedia.component';
 export class SignupComponent implements OnInit {
   base64DP = '';
 
-  constructor(private firebaseService: FirebaseService,
+  constructor(private snackBar: MatSnackBar, private firebaseService: FirebaseService,
               private encodeService: EncoderService) { }
 
   ngOnInit() {
@@ -24,7 +24,16 @@ export class SignupComponent implements OnInit {
     this.firebaseService.createUser(value.userId, value.name, value.password, this.base64DP);
   }
 
-  UploadProfilePicture() {
-    // this.base64dp = encoder service return base64 string
+  async OpenImage(event: any) {
+    // The file is in this event
+    console.log(event.target.files[0].name);
+    // this.encodeService.Base64EncodeImage(event.target.files[0], event.target.files[0].name, this.data.UserId, this.data.ContactId, this.data.ActiveContact);
+    this.openSnackBar('Icon Uploaded', 'Close');
+  }
+
+  openSnackBar(message: string, action: string) {
+    this.snackBar.open(message, action, {
+      duration: 2000,
+    });
   }
 }
